a6e792add3e2fde3f4427ae09ef86e78ee20091c
ShannonTao <shannon.cxq@gmail.com>
2022-07-06 09:23:24 -0400

    added scroll to top button and link on footer

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ const closeMenu = document.querySelector("#closeMenu");
 const nextImage = document.querySelector("#nextImage");
 const prevImage = document.querySelector("#prevImage");
 const headerNavBar = document.querySelector(".headerNavBar");
+const headerWrapper = document.querySelector(".homeHeader .wrapper");
+const scrollToTop = document.querySelector(".scrollToTop");
 
-document.addEventListener("scroll", function () {
-  headerNavBar.classList.add("active");
+document.addEventListener("scroll", function (e) {
+  console.log(e);
+
+  if (scrollY > 10) {
+    headerNavBar.classList.add("active");
+    headerWrapper.classList.add("active");
+    scrollToTop.style.display = "block";
+  } else {
+    headerNavBar.classList.remove("active");
+    headerWrapper.classList.remove("active");
+    scrollToTop.style.display = "none";
+  }
+});
+
+scrollToTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
 });
 
 openSideNav.addEventListener("click", function () {
@@ -24,6 +40,8 @@ nextImage.addEventListener("click", function () {
 prevImage.addEventListener("click", function () {
   console.log("prev image pressed");
 });
+
+console.log(headerWrapper);
 
 // carousel if possible
 // sticky nav bar
