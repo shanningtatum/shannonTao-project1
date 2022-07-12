46ed29d55f40be9432c64d8599d7b7a171f79a48
ShannonTao <shannon.cxq@gmail.com>
2022-07-11 22:47:57 -0400

    added active pages text decoration

--- script.js
+++ script.js
@@ -35,91 +35,108 @@ closeMenu.addEventListener("click", function () {
 
 const formEl = document.querySelector("form");
 
-formEl.addEventListener("submit", function (e) {
-  e.preventDefault();
+const setupEventListener = () => {
+  formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  // create Date
-  const date = new Date();
-  const day = date.getDate();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "November",
-    "December",
-  ];
-  // store the data the user is posting the comment
-  const postDate = `${months[month]}  ${day}  ${year}`;
+    // create Date
+    const date = new Date();
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "November",
+      "December",
+    ];
+    // store the data the user is posting the comment
+    const postDate = `${months[month]}  ${day}  ${year}`;
 
-  // store user name
-  const userNameInput = document.querySelector("#name");
-  const userName = userNameInput.value;
+    // store user name
+    const userNameInput = document.querySelector("#name");
+    const userName = userNameInput.value;
 
-  // store user comment
-  const userCommentInput = document.querySelector("#userComment");
-  const userComment = userCommentInput.value;
+    // store user comment
+    const userCommentInput = document.querySelector("#userComment");
+    const userComment = userCommentInput.value;
 
-  if (userName && userComment) {
-    // create li element for our UL
-    const listEl = document.createElement("li");
-    const userPost = document.createElement("div");
-    userPost.classList.add("commentText");
+    if (userName && userComment) {
+      // create li element for our UL
+      const listEl = document.createElement("li");
+      const userPost = document.createElement("div");
+      userPost.classList.add("commentText");
 
-    // creates div class with default pfp
-    listEl.innerHTML = `<div class="commentPicture"><img src="./assets/blog-8.png" alt="Default User Profile Picture"/>`;
+      // creates div class with default pfp
+      listEl.innerHTML = `<div class="commentPicture"><img src="./assets/blog-8.png" alt="Default User Profile Picture"/>`;
 
-    // create div element
-    const divEl = document.createElement("div");
-    divEl.classList.add("commentText");
+      // create div element
+      const divEl = document.createElement("div");
+      divEl.classList.add("commentText");
 
-    // create h3 class with user name value
-    const h3El = document.createElement("h3");
+      // create h3 class with user name value
+      const h3El = document.createElement("h3");
 
-    h3El.textContent = userName;
-    const postName = h3El.textContent;
+      h3El.textContent = userName;
+      const postName = h3El.textContent;
 
-    // how to Include REPLY type?
-    h3El.innerHTML = `${postName} - <span class="responseType">Reply</span>`;
+      // how to Include REPLY type?
+      h3El.innerHTML = `${postName} - <span class="responseType">Reply</span>`;
 
-    // create paragraph element
-    const paragraphEl = document.createElement("p");
+      // create paragraph element
+      const paragraphEl = document.createElement("p");
 
-    paragraphEl.textContent = userComment;
+      paragraphEl.textContent = userComment;
 
-    // create second paragraph element
-    const dateParagraphEl = document.createElement("p");
-    dateParagraphEl.classList.add("postDate");
+      // create second paragraph element
+      const dateParagraphEl = document.createElement("p");
+      dateParagraphEl.classList.add("postDate");
 
-    dateParagraphEl.textContent = postDate;
+      dateParagraphEl.textContent = postDate;
 
-    // append h3 and p to div
-    divEl.append(h3El);
-    divEl.append(paragraphEl);
-    divEl.append(dateParagraphEl);
+      // append h3 and p to div
+      divEl.append(h3El);
+      divEl.append(paragraphEl);
+      divEl.append(dateParagraphEl);
 
-    // clear the userName and userComment input fields
-    userNameInput.value = "";
-    userCommentInput.value = "";
+      // clear the userName and userComment input fields
+      userNameInput.value = "";
+      userCommentInput.value = "";
 
-    // <div class="commentPicture"><img src="./assets/blog-8.jpeg" alt="Default User Profile Picture"/></div>
+      // <div class="commentPicture"><img src="./assets/blog-8.jpeg" alt="Default User Profile Picture"/></div>
 
-    commentPost.append(listEl);
-    listEl.append(divEl);
-  } else {
-    alert("please enter name and message!");
-  }
-});
+      commentPost.append(listEl);
+      listEl.append(divEl);
+    } else {
+      alert("please enter name and message!");
+    }
+  });
+};
+
+// active class
+const path = window.location.href;
+const navLink = document.querySelectorAll("header a");
+
+// console.log(navLink);
+// console.log(path);
+
+// navLink.forEach(function () {
+//   console.log(navLink.href);
+// });
+
+// PSEUDO CODE FOR SEARCH BAR
+// users click the search icon
+// search input displays behind it
+// select search icon and add an event listener for when it is clicked
 
 // PSEUDO CODE FOR NAV BAR
-// create function for
 
 // PSEUDO CODE FOR COMMENT SECTION
 // create event Listener for submit button
